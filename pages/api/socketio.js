b9811fb2b099338ece4155da6b30a035a582f25b
PoJop <kostetzkiy.oleg@gmail.com>
2022-07-17 18:11:01 +0300

    cors

--- pages/api/socketio.js
+++ pages/api/socketio.js
@@ -1,22 +1,40 @@
 import { Server as ServerIO } from "socket.io";
+import Cors from 'cors'
+const cors = Cors({
+    origin: "*",
+    methods: ['POST', 'GET', 'HEAD'],
+})
+function runMiddleware(req, res, fn) {
+    return new Promise((resolve, reject) => {
+        fn(req, res, (result) => {
+            if (result instanceof Error) {
+                return reject(result)
+            }
+            console.log(req)
+            if (!res.socket.server.io) {
+                console.log("New Socket.io server...");
+                // adapt Next's net Server to http Server
+                const httpServer = res.socket.server;
+                const io = new ServerIO(httpServer, {
+                    path: "/api/socketio",
+                    cors: {
+                        origin: "*",
+                        methods: ["GET", "POST"]
+                    }
+                });
+                res.socket.server.io = io;
+                io.on("connection", (socket) => {
+                    console.log("connection")
+                });
+            }
+            res.end();
+            return resolve(result)
+        })
+    })
+}
 
 export default function handler(req, res) {
-    console.log(req)
-    if (!res.socket.server.io) {
-        console.log("New Socket.io server...");
-        // adapt Next's net Server to http Server
-        const httpServer = res.socket.server;
-        const io = new ServerIO(httpServer, {
-            path: "/api/socketio",
-            cors: {
-                origin: "http://localhost:3000",
-                methods: ["GET", "POST"]
-              }
-        });
-        res.socket.server.io = io;
-        io.on("connection", (socket) => {
-            console.log("connection")
-        });
-    }
-    res.end();
+
+    await runMiddleware(req, res, cors)
+
 }
