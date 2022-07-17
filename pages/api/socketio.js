aa1a3b95e068e5aa3bc6fd73d5564125a1bf1f87
PoJop <kostetzkiy.oleg@gmail.com>
2022-07-17 20:30:33 +0300

    cors

--- pages/api/socketio.js
+++ pages/api/socketio.js
@@ -1,30 +1,28 @@
 import { Server as ServerIO } from "socket.io";
 
 export default function handler(req, res) {
-    if (!res.socket.server.io) {
-
         console.log("New Socket.io server...");
-
+    if (!res.socket.server.io) {
+        // adapt Next's net Server to http Server
         const httpServer = res.socket.server;
         const io = new ServerIO(httpServer, {
             path: "/api/socketio",
-            origins: ["http://localhost:8080"],
+            origin: "*",
 
             handlePreflightRequest: (req, res) => {
-                res.writeHead(200, {
-                    "Access-Control-Allow-Origin": " *",
-                    "Access-Control-Allow-Methods": "*",
-                    "Access-Control-Allow-Headers": "my-custom-header",
-                    "Access-Control-Allow-Credentials": true
-                });
-                res.end();
+              res.writeHead(200, {
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Methods": "*",
+                "Access-Control-Allow-Headers": "my-custom-header",
+                "Access-Control-Allow-Credentials": true
+              });
+              res.end();
             }
         });
         res.socket.server.io = io;
         io.on("connection", (socket) => {
             console.log("connection")
         });
-
     }
     res.end();
 }
