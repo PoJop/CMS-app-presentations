29977d7314da64e7f72c2cdcd591b244b89b2015
PoJop <kostetzkiy.oleg@gmail.com>
2022-07-17 22:25:07 +0300

    socket

--- pages/api/socketio.js
+++ pages/api/socketio.js
@@ -1,28 +1,20 @@
 import { Server as ServerIO } from "socket.io";
 
-export default function handler(req, res) {
-        console.log("New Socket.io server...");
-    if (!res.socket.server.io) {
-        // adapt Next's net Server to http Server
-        const httpServer = res.socket.server;
-        const io = new ServerIO(httpServer, {
-            path: "/api/socketio",
-            origin: "*",
-
-            handlePreflightRequest: (req, res) => {
-              res.writeHead(200, {
-                "Access-Control-Allow-Origin": "*",
-                "Access-Control-Allow-Methods": "*",
-                "Access-Control-Allow-Headers": "my-custom-header",
-                "Access-Control-Allow-Credentials": true
-              });
-              res.end();
-            }
-        });
-        res.socket.server.io = io;
-        io.on("connection", (socket) => {
-            console.log("connection")
-        });
-    }
-    res.end();
-}
+const SocketHandler = (req, res) => {
+    if (res.socket.server.io) {
+      console.log('Socket is already running')
+    } else {
+        console.log('Socket is initializing')
+        const io = new Server(res.socket.server)
+        res.socket.server.io = io
+    
+        io.on('connection', socket => {
+          socket.on('input-change', msg => {
+            socket.broadcast.emit('update-input', msg)
+          })
+        })
+      }
+    res.end()
+  }
+  
+  export default SocketHandler
